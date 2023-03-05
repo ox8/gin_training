68499e50460b010085832a749e49e16c423ff9fe
Ryota <abryt69@gmail.com>
2023-03-05 17:20:59 +0900

    chore: add the worse test code

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
+	r = gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "WOW, SUCCESS!!!!",
